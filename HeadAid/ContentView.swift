5df35a2a1f1a4d62f15c2d5641472ce0906f6ac4
Gracious O. Asare <gpoa@bu.edu>
2024-08-13 17:08:21 -0400

    Initial Commit

---
+++ HeadAid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeadAid
+//
+//  Created by Gracious Ogyiri Asare on 8/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeadAidUITests/HeadAidUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HeadAidUITestsLaunchTests.swift
+//  HeadAidUITests
+//
+//  Created by Gracious Ogyiri Asare on 8/13/24.
+//
+
+import XCTest
+
+final class HeadAidUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
